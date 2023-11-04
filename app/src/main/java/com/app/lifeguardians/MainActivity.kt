44556734e0f631a8da0e1d06c8a91522c301d230
choizzz <choizzz@dawonelectron.com>
2023-11-04 20:52:12 +0900

    좌표 받아오기 성공

--- app/src/main/java/com/app/lifeguardians/MainActivity.kt
+++ app/src/main/java/com/app/lifeguardians/MainActivity.kt
@@ -2,14 +2,21 @@ package com.app.lifeguardians
 
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.Manifest.permission.ACCESS_FINE_LOCATION
-import android.graphics.PointF
+import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.nfc.Tag
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.LocationSource
+import androidx.fragment.app.FragmentActivity
+import com.app.lifeguardians.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
@@ -19,25 +26,7 @@ import com.naver.maps.map.util.FusedLocationSource
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private class CustomLocationSource : LocationSource, NaverMap.OnMapClickListener {
-        private var listener: LocationSource.OnLocationChangedListener? = null
 
-        override fun activate(listener: LocationSource.OnLocationChangedListener) {
-            this.listener = listener
-        }
-
-        override fun deactivate() {
-            listener = null
-        }
-
-        override fun onMapClick(point: PointF, coord: LatLng) {
-            listener?.onLocationChanged(Location("CustomLocationSource").apply {
-                latitude = coord.latitude
-                longitude = coord.longitude
-                accuracy = 100f
-            })
-        }
-    }
 
     private val PERMISSIONS = arrayOf<String>(
         ACCESS_FINE_LOCATION,
@@ -46,14 +35,28 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var locationManager: LocationManager
+    private lateinit var locationListener: LocationListener
+
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         supportActionBar?.let {
             it.setDisplayHomeAsUpEnabled(true)
             it.setDisplayShowHomeEnabled(true)
         }
+        binding.fabAdd.setOnClickListener {
+            //Toast.makeText(this, "add button clicked", Toast.LENGTH_LONG).show()
+            getLastLocation()
+        }
+
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment?
             ?: MapFragment.newInstance(NaverMapOptions().locationButtonEnabled(true)).also {
@@ -61,6 +64,36 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         mapFragment.getMapAsync(this)
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+
+
+        locationListener = LocationListener { location ->
+            val latitude = location.latitude
+            val longitude = location.longitude
+            Toast.makeText(this, "Latitude: $latitude, Longitude: $longitude", Toast.LENGTH_LONG).show()
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+    }
+    private fun getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), REQUEST_LOCATION)
+            return
+        }
+
+        val task: Task<Location> = fusedLocationClient.lastLocation
+        task.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                val latitude = location.latitude
+                val longitude = location.longitude
+                Toast.makeText(this, "Latitude: $latitude, Longitude: $longitude", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Location not available", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int,
@@ -85,6 +118,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+        private const val REQUEST_LOCATION = 1
     }
 
 }
